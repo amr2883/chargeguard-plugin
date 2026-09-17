222008efc8ce4deeb6ae4e7a20bb773a6e3b9514
Amr Sayed <amrsayed18323@gmail.com>
2026-09-17 20:11:12 +0300

    chore: bump version to 1.0.30

--- chargeguard-woocommerce.php
+++ chargeguard-woocommerce.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: ChargeGuard for WooCommerce
  * Description: Advanced Card Testing prevention powered by ChargeGuard intelligence.
- * Version:     1.0.28
+ * Version:     1.0.30
  * Author:      ChargeGuard
  * Author URI:  https://chargeguard.io
  * License:     GPLv2 or later
